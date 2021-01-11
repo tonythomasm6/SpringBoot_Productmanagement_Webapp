52f8894a2a7db8f53965bd06ac93515f36a710ab
Tony <tonythomasm6@gmail.com>
2021-01-11 20:17:48 +1100

    Basic app setup

--- src/main/java/com/springboot_productmanagement_webapp/productmanagement_webapp/ProductmanagementWebappApplication.java
+++ src/main/java/com/springboot_productmanagement_webapp/productmanagement_webapp/ProductmanagementWebappApplication.java
@@ -2,7 +2,8 @@ package com.springboot_productmanagement_webapp.productmanagement_webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Created by Tony Thomas
+//11-Jan-2021
 @SpringBootApplication
 public class ProductmanagementWebappApplication {
 
